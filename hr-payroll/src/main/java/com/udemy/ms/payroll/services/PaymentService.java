6416b606804b6c5d1a2075948640324897fb1901
Lucas Ferreira <lucasraphael.fernandes@hotmail.com>
2021-04-26 10:59:59 -0300

    Adicionando payment service, resttemplate

--- hr-payroll/src/main/java/com/udemy/ms/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/ms/payroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.udemy.ms.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemy.ms.payroll.entities.Payment;
+import com.udemy.ms.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Nome", 100.0, days);
+		Map<String, String> params = 	new HashMap<>();
+		
+		params.put("id", String.valueOf(workerId));
+		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, params);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
