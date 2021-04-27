456f9bf47827637783504653d5ad93b32ef8b60b
Lucas Ferreira <lucasraphael.fernandes@hotmail.com>
2021-04-27 08:31:50 -0300

    Adicionando FeignClient

--- hr-payroll/src/main/java/com/udemy/ms/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/ms/payroll/services/PaymentService.java
@@ -1,31 +1,20 @@
 package com.udemy.ms.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.udemy.ms.payroll.entities.Payment;
 import com.udemy.ms.payroll.entities.Worker;
+import com.udemy.ms.payroll.interfaces.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient feignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> params = 	new HashMap<>();
-		
-		params.put("id", String.valueOf(workerId));
-		Worker worker = restTemplate.getForObject(workerHost + "workers/{id}", Worker.class, params);
-		
+		Worker worker = feignClient.getById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
