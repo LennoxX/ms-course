64a4456fa68adeb98243912c61d4085a9b4f8003
Lucas Ferreira <lucasraphael.fernandes@hotmail.com>
2021-04-28 10:02:20 -0300

    Criando hr-config-server

---
+++ hr-config-server/src/test/java/com/udemy/ms/config/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.ms.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
